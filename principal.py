a48d8e8b4ca5a7a1a1d33daec79cd3877980e490
adrian colman <adriangamarra093@gmail.com>
2024-08-28 11:53:51 -0300

    hice la clase jugador y lo probe con el archivo principal

--- principal.py
+++ principal.py
@@ -1,11 +1,13 @@
 from perinola import Perinola
 from apuestas import Apuesta
+from jugador import Jugador
 
-p = Perinola()
-p.tirar()
-print(p.cara_visible)
+#p = Perinola()
+#p.tirar()
+#print(p.cara_visible)
 
+#A = Apuesta()
+#print(A)
 
-A = Apuesta()
-A.tomarFichas(5)
-print(A)
+#J = Jugador("Adrián, 26")
+#print(J)
